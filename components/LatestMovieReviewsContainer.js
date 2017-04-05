966d1427b29588d66fefda953f8d35b05ae574a4
Benjamin Cantlupe <bjcantlupe@gmail.com>
2017-04-05 19:37:30 +0000

    Done.

--- components/LatestMovieReviewsContainer.js
+++ components/LatestMovieReviewsContainer.js
@@ -9,7 +9,33 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 class LatestMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentWillMount() {
+    fetch(URL)
+      .then(function(response) {
+        return response.json()
+      }).then(function(json) {
+        this.setState({ reviews: json });
+      }).catch(function(ex) {
+        console.log('parsing failed', ex)
+      });
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <h2>The Latest Movie Reviews</h2>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
 }
 
 module.exports = LatestMovieReviewsContainer;
--- components/MovieReviews.js
+++ components/MovieReviews.js
@@ -1,22 +1,31 @@
 const React = require('react');
 
-const MovieReviews = ({ results }) => {
+const MovieReviews = ({ reviews }) => {
   return (
     <div className="review-list">
       {
-        results.map({
-          display_title,
+        reviews.map(({
           byline,
           headline,
-          summary_short,
-          link} => {
-            return (
-              <div className="review" key={display_title}></div>
-            );
-          })
+          link,
+          summary_short
+        }) => {
+          return (
+              <div key={headline} className="review">
+                <a href={link.url}>{headline}</a>
+                <br/>
+                <span className="author"><strong>{byline}</strong></span>
+                <blockquote>{summary_short}</blockquote>
+              </div>
+          );
+        })
       }
     </div>
   );
-}
+};
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
 
 module.exports = MovieReviews;
