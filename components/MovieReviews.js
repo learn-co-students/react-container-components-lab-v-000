60f8fa6ae3b9cc3fc8411615fcac2ca4c987939b
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-04-28 07:29:37 +0000

    Done.

---
+++ components/MovieReviews.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+  return (
+    <div className='review-list'>
+    {
+      reviews.map((r, index) => (
+        <div className='review' key={index}>Review</div>
+      ))
+    }
+    </div>
+  )
+}
+
+MovieReviews.defaultProps = {
+  reviews: ['Hello']
+}
+
+module.exports = MovieReviews
