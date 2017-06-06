16c91ff57583bfea6bea8f664a25986c29e6a1c0
github username <krishel.lasam@gmail.com>
2017-06-06 05:14:49 -0400

    Done.

---
+++ components/LatestMovieReviewsContainer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import MovieReviews from './MovieReviews'
+const { fetch } = require('whatwg-fetch')
+
+export default class LatestMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentWillMount() {
+    fetch('https://api.nytimes.com/svc/movies/v2/reviews/all.json')
+      .then((res => this.setState({ reviews: res.results })))
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
