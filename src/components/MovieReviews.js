7930fdc87993767b5996ad39028c24b37bf626ac
yianna <yiannakokalas@gmail.com>
2017-06-20 19:25:26 -0400

    😂

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,34 @@
-// Code MovieReviews Here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+const Review = (
+  { 
+    headline,
+  byline,
+  link,
+  summary_short 
+  }) => {
+    return(
+      <div key={headline} className="review">
+      <header>
+      <a className="review-link" href={link.url}>
+      {headline}
+      </a>
+      <br/>
+      <span className="author">{byline}</span>
+      </header>
+      <blockquote>{summary_short}</blockquote>
+      </div>
+    )
+  }
+
+const MovieReviews = ({ reviews }) => {
+  return (
+    <div className="review-list">
+      { reviews.map(Review) }
+    </div>
+  )
+}
+
+MovieReviews.defaultProps = { reviews: [] }
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,38 @@
-// Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import MovieReviews from './MovieReviews';
+
+class SearchableMovieReviewsContainer extends Component {
+  constructor() {
+    super();
+ 
+    this.state = {
+      searchTerm: '',
+      reviews: []
+    };
+
+    this.handleSearchInputChange = this.handleSearchInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSearchInputChange(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
+
+
+  handleSubmit(e) {
+    e.preventDefault();
+    fetch(BASE_URL.concat(this.state.searchTerm))
+      .then(res => res.json())
+      .then(res => this.setState({ reviews: res.results }));
+  }
+
+  render() {
+    return(
+      <div className="searchable-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+} 
+
+export default SearchableMovieReviewsContainer;
