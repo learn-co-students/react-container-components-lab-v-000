12e72910d7c37f143277ffa7bc78bce180f2fe08
briecoyle <brie.coyle@gmail.com>
2017-06-27 12:07:42 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,24 @@
 // Code MovieReviews Here
+import React from 'react';
+
+function formatReview(review) {
+  return (
+    <div className="review">
+      <h3>{review.headline}</h3>
+      <h5>By: {review.byline}</h5>
+      <p>{review.summary_short}</p>
+    </div>
+  )
+}
+
+export default function MovieReviews({reviews}) {
+  return (
+      <div className="review-list">
+        {reviews.map(formatReview)}
+      </div>
+    )
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,47 @@
 // Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import MovieReviews from './MovieReviews.js'
+import 'isomorphic-fetch';
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+            + `api-key=${NYT_API_KEY}&query=`;
+
+// Code LatestMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchTerm: '',
+      reviews: [],
+    }
+
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.handleUpdateInput = this.handleUpdateInput.bind(this)
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    fetch(URL + this.state.searchTerm)
+      .then(res => res.json())
+      .then(res => this.setState({reviews: res.results}))
+  }
+
+  handleUpdateInput(event) {
+    this.setState({searchTerm: event.target.value})
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleFormSubmit}>
+          <input type="text" value={this.state.searchTerm} onChange={this.handleUpdateInput}/>
+          <button>Search</button>
+        </form>
+        <h1>Search Results:</h1>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
