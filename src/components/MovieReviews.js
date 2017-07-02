6cbe3e227e6c82ea23afb7cd0cd407c88d5049bd
Callie Brazil <cbrazil@gmail.com>
2017-07-02 08:52:51 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
 // Code MovieReviews Here
+import React from 'react';
+
+
+function formatReview(review){
+  return(
+    <div className="review">
+      <h2>{review.headline}</h2>
+      <h3>Created By: {review.byline}</h3>
+      <p>{review.summary_short}</p>
+    </div>
+  )
+}
+
+export default function MovieReviews({reviews}){
+  return(
+    <div className="review-list">
+      {reviews.map(formatReview)}
+    </div>
+  )
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
