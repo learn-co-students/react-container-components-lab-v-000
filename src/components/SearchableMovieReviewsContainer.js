260676f8cb61f3894012b467d89e18a16771d92a
Menachem Hornbacher <mendelh1537@gmail.com>
2017-07-19 20:18:25 -0400

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,9 +4,7 @@ import 'isomorphic-fetch';
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const GENERATE_SEARCH_URL = (query) => {
-    'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-    + `api-key=${NYT_API_KEY}`
-    + `query=${query}`
+    'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' + `api-key=${NYT_API_KEY}` + `query=${query}`
 }
 // Code SearchableMovieReviewsContainer Here
 
