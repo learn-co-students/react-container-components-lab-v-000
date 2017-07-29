9b101e56d7f4adf80be24587d28635a69a272592
Trai Compton <tlcompton2546@outlook.com>
2017-07-29 00:05:58 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,29 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const review = ({
+    headline,
+    byline,
+    summary_short,
+    link
+}) => {
+    return(
+        <div className='review'>
+            <h2>{ headline }</h2>
+            <h3>{ byline }</h3>
+            <a href={ link.url }>{ link.suggested_link_text }</a>
+            <p>{ summary_short }</p>
+        </div>
+    );
+};
+
+const MovieReviews = ({ reviews }) => (
+    <div className='review-list'>
+        {reviews.map(review)}
+    </div>
+);
+
+MovieReviews.defaultProps = {
+    reviews: []
+};
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,76 @@
-// Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
+import PropTypes from 'prop-types';
+
+import MovieReviews from './MovieReviews';
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' + `api-key=${NYT_API_KEY}?query=`;
+
+const searchTerm = ({ 
+    label,
+    handleChange,
+    handleSubmit
+    
+}) => {
+    return (
+        <form onSubmit={ this.handleSubmit }>
+            <label for='search-input'>{ label }</label>
+            <input name='search-input' type='text' onChange={ this.handleChange }/>
+            <input type='submit'/>
+        </form>
+    );
+};
+
+class SearchableMovieReviewsContainer extends Component {
+    constructor(){
+        super();
+        
+        this.state ={
+            searchTerm: '',
+            reviews: []   
+        };
+        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(e){
+        this.setState({
+            searchTerm: e.target.value
+        });
+    }
+    
+    handleSubmit(e){
+        e.preventDefault();
+        fetch(URL.concat(this.state.searchTerm))
+        .then(resp => resp.json())
+        .then(resp => this.setState({
+            reviews: resp.results
+        }));
+    }
+    
+    render(){
+        return(
+            <div className='searchable-movie-reviews'>
+                <searchTerm 
+                    label= 'search'
+                    handleSubmit={ this.handleSubmit }
+                    handleChange={ this.handleChange }
+                />
+                <MovieReviews reviews={ this.state.reviews } />
+            </div>
+        );
+    }
+}
+
+searchTerm.defaultProps = {
+    label: 'Search'
+};
+
+searchTerm.propTypes ={
+    handleChange: React.PropTypes.func,
+    handleSubmit: React.PropTypes.func
+};
+
+export default SearchableMovieReviewsContainer;
