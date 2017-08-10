3da7d3b5d5c19aa7fb19f330804089e9f4b4d931
Isis Tejeda <isis@isistejeda.com>
2017-08-10 17:37:47 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,27 @@
-// Code MovieReviews Here
+
+import React from 'react';
+
+const MovieReviews = ({ reviews }) => {
+    return <div className="review-list">
+        {reviews.map(renderReview)}
+        </div>
+    };
+
+    MovieReviews.defaultProps = {
+    reviews: []
+};
+
+
+const renderReview = ({
+  headline,
+  byline,
+  link,
+  summary_short}) => {
+  return (
+    <div className="review">
+    </div>
+  );
+};
+
+
+export default MovieReviews;
