f7a3ba6f549bec835a339a610f17a0cea9688c96
szeidman <sam.zeidman@gmail.com>
2017-09-08 22:11:32 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,24 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const fromReview = ({ summary_short, display_title }) => {
+  return (
+    <div key={display_title} className="review">
+      <p>{ summary_short }</p>
+    </div>
+  )
+}
+
+const MovieReviews = ({reviews}) => {
+    return (
+      <div className="review-list">
+       {reviews.map(fromReview)}
+      </div>
+    )
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews;
