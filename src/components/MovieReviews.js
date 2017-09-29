3252b67e8e51ef7f24c44d56af977852d5682c04
Cara Kane <cara.kane@gmail.com>
2017-09-29 16:51:40 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+import React from 'react'
+
+class MovieReviews extends React.Component{
+  render () {
+
+    return(
+      <div class="review-list">
+        {this.props.reviews.map((review) =>
+          <div class="review">{review.headline}
+        </div>
+      )}
+      </div>
+    )
+  }
+
+};
+
+export default MovieReviews;
