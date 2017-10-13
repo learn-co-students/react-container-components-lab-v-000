443f24d8056666e3c92ef6e97b1be2142dc76a90
christinechoi <smi77y@gmail.com>
2017-10-12 20:38:18 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -20,7 +20,7 @@ class LatestMovieReviewsContainer extends Component {
   render() {
     return (
       <div className="latest-movie-reviews">
-
+        {this.state.reviews}
       </div>
     )
   }
