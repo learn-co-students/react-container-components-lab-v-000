b72870322f61adfd156a0f4d80b7a0cdbb3cfaa2
christinechoi <smi77y@gmail.com>
2017-10-14 21:54:22 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -6,18 +6,25 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+
+const MovieReview = ({ review }) => {
+  return (
+    <div className="review">
+      <h3>{review}</h3>
+    </div>
+  )
+}
+
 
 class MovieReviewsContainer extends Component {
-   constructor(props) {
-    super(props);
-  }
+  //  constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return (
       <div className="review-list">
-        
-        <h3>{reviews}</h3>
+        {movieReviews.map(movieReview => <MovieReview review="movieReview.review" />)}
       </div>
     )
   }
