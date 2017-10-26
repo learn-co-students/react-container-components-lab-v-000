07d8b3415f2a99724dbc0ed938e6443e1e083e32
will-sauer-learn <will@heueso.com>
2017-10-26 12:45:44 -0400

    latest movies done

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+  return(
+    <div className="review-list">
+          {reviews.map(review => 
+            <div className="review"> 
+              <h3>{review.headline}</h3>
+              <h5>{review.byline}</h5>
+              <p>{review.summary_short}</p>
+            </div>
+          )}
+    </div>
+  )
+}
+
+export default MovieReviews
+
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,4 @@
 // Code SearchableMovieReviewsContainer Here
+//import react and moviereviews
+//in the class create an input that upates - each time re-calling the fetch
+//updating state.reviews and thereby re-rendering
