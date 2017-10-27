d88f3140f937b8b56b7db97776fd7f3a7d277bd3
Timothy Cevallos <timcev@gmail.com>
2017-10-27 15:46:40 +0000

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,26 @@
 // Code SearchableMovieReviewsContainer Here
+import React from 'react';
+import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
+
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      reviews: [],
+      searchTerm: "",
+    };
+  }
+  render () {
+    return (
+      <div className="searchable-movie-reviews">
+        <MovieReviews
+          reviews = {this.state.reviews}
+          />
+      </div>
+    );
+  }
+}
+
+export default SearchableMovieReviewsContainer;
