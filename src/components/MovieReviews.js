bfdd5e2bc8d3561c2f3d219b51a2da128e092c8f
tpstar <hanlee12@gmail.com>
2017-11-05 22:56:00 -0600

    11/12

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({reviews}) => {
+  const movieReviews = reviews.map((review, index) => {
+    return (<div className="review" key={index}>{review.display_title}</div>)
+  })
+
+  return (
+    <div className="review-list">
+      {movieReviews}
+    </div>
+  )
+}
+
+MovieReviews.defaultProps = {reviews: []}
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,20 @@
 // Code SearchableMovieReviewsContainer Here
+import React from 'react';
+
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviews: this.props.reviews,
+      searchTerm: ''
+    }
+  }
+  render() {
+    return (
+      <div className="searchable-movie-reviews"></div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer;
