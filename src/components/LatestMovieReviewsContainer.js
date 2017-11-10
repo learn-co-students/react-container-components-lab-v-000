3de98cd418a5b8024d8ed0d2e3441b617b723231
smithwebtek <brad@smithwebtek.com>
2017-11-10 18:29:49 -0500

    6 passing

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,52 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const NYT_API_KEY = 'b0de5a02334245c396bd7a33ba979bf1';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+const Review = ({ display_title, headline, summary_short, src  }) => {
+  return (
+    <div className="review">
+      <h4>{display_title}</h4>
+      <h5>{headline}</h5>
+      <h5>{summary_short}</h5>
+      <img src={src}/>
+      <hr />
+    </div>
+  )
+}
+
+const ReviewList = ({ reviews }) => (
+  <div className="review-list"> 
+    { reviews.map(review => <Review 
+      display_title= {review.display_title} 
+      headline= {review.headline} 
+      summary_short= {review.summary_short} 
+      src= {review.multimedia.src}
+      /> )}
+  </div>
+);
+
+class LatestMovieReviewsContainer extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      reviews: [],
+    };
+  }
+ 
+  componentWillMount() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(response => this.setState({ reviews: response.results }));
+  }
+  
+  render() {
+    return (
+    <ReviewList className="latest-movie-reviews" reviews={this.state.reviews} />
+  )}
+}
+
+export default LatestMovieReviewsContainer;
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,49 @@
-// Code MovieReviews Here
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+
+const NYT_API_KEY = 'b0de5a02334245c396bd7a33ba979bf1';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+            + `api-key=${NYT_API_KEY}`;
+
+const Review = ({ display_title, headline, summary_short, src  }) => {
+  return (
+    <div className="review">
+      <h4>{display_title}</h4>
+      <h5>{headline}</h5>
+      <h5>{summary_short}</h5>
+      <img src={src}/>
+      <hr />
+    </div>
+  )
+}
+
+const ReviewList = ({ reviews }) => (
+  <div className="review-list"> 
+    { reviews.map(review => <Review 
+      display_title= {review.display_title} 
+      headline= {review.headline} 
+      summary_short= {review.summary_short} 
+      src= {review.multimedia.src}
+      /> )}
+  </div>
+);
+
+const reviews = () => {
+  fetch(URL)
+    .then(res => res.json())
+    .then(response => { return response.results });
+}
+
+class MovieReviews extends Component {
+  render() {
+    return (
+      <ReviewList className="review-list" reviews={reviews} />
+    )
+  }
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews;
