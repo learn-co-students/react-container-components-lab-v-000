3d2188a0eb8fa18aec11b0f18fcfafb6297818f7
laramontana <larabrightside@gmail.com>
2017-12-02 03:09:05 +0300

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,26 @@
-// Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import MovieReviews from './MovieReviews';
+
+import 'isomorphic-fetch';
+
+const NYT_API_KEY = '26ecb4626a4149b99a280d526b3910eb';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+            + `api-key=${NYT_API_KEY}`;
+
+class SearchableMovieReviewsContainer extends React.Component {
+
+
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+      </div>
+
+    )
+
+  }
+
+
+}
+
+export default SearchableMovieReviewsContainer;
