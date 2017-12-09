1bbb1a2460df173428a4f3586e6683ad86d8e6df
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-09 12:04:44 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const Review = ({ display_title, byline, summary_short }) => {
+  return (
+    <div className='review'>
+      <h1>{ display_title }</h1>
+      <h3>{ byline }</h3>
+      <p>{ summary_short }</p>
+    </div>
+  )
+}
+
+const MovieReviews = ({ reviews }) => {
+  return (
+    <div className='review-list'>
+      { reviews.map(Review) }
+    </div>
+  );
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews;
