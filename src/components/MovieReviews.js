541e07afb7a8df259c63149fdf84441ecdc3d72b
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-12-09 19:15:40 -0800

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = ({reviews}) => (
+  <ul className="review-list">
+    {reviews.map((rev) => <li className="review">{rev.mpaa_rating} - {rev.byline}</li>)}
+  </ul>
+)
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews
