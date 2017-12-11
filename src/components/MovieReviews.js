dd723f9c6d9c2e024c706f66aaa84fe750b0e574
github username <github email address>
2017-12-11 14:48:21 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,29 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({ reviews }) => (
+	<ol className="review-list">
+		{reviews.map(review => <li className="review">{review.display_title}</li>)}
+	</ol>
+)
+
+MovieReviews.defaultProps = {
+	reviews: []
+}
+
+export default MovieReviews
+
+
+
+// import React from 'react'
+
+// const MovieReviews = ({reviews}) => (
+//   <ul className="review-list">
+//     {reviews.map((rev) => <li className="review">{rev.mpaa_rating} - {rev.byline}</li>)}
+//   </ul>
+// )
+
+// MovieReviews.defaultProps = {
+//   reviews: []
+// }
+
+// export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,44 @@
-// Code SearchableMovieReviewsContainer Here
+import React from 'react';
+import MovieReviews from './MovieReviews'
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+						+ `api-key=${NYT_API_KEY}` + 'query='
+
+export default class SearchableMovieReviewsContainer extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			reviews: [],
+			searchTerm: ""
+		}
+
+	}
+
+	handleChange = (e) => {
+		this.setState({ searchTerm: e.target.value })
+	}
+
+	fetchMovies = (e) => {
+		e.preventDefault()
+			
+		fetch(URL.concat(this.state.searchTerm))
+			.then(response => response.json())
+				.then(data => this.setState({ reviews: data.results }))	
+
+
+	}
+
+	render() {
+		return (
+			<div className="searchable-movie-reviews">
+				<form onSubmit={this.fetchMovies}>
+					<input type="text" onChange={this.handleChange}></input>
+					<button type="submit">Submit</button>
+				</form>
+			<MovieReviews reviews={ this.state.reviews } />
+			</div>
+		)
+	}
+}
