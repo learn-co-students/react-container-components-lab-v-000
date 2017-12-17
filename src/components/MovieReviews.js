fd4b14dda1c6f4f514831a28302dab6946747f96
Sophia Hubeny <shhubeny@gmail.com>
2017-12-17 17:34:28 +0000

    progress

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,13 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({reviews}) => (
+  <div className = "review-list">
+
+  </div>
+)
+
+MovieReviews.defaultProps = {
+  reviews:[]
+}
+export default MovieReviews
