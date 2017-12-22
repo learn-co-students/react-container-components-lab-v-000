42b3a7f6090cfbbe4e8e0fb9e754b2e37ceed890
Logan McAnsh <logan@mcan.sh>
2017-12-22 14:27:10 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,27 @@
-// Code MovieReviews Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MovieReviews = ({ reviews }) => (
+  <ul className="review-list">
+    {reviews.map(review => (
+      <li className="review" key={review.display_title}>
+        {review.mpaa_rating || 'NR'} - {review.byline}
+      </li>
+    ))}
+  </ul>
+);
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+MovieReviews.propTypes = {
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape({
+      byline: PropTypes.string,
+      mpaa_rating: PropTypes.string
+    })
+  )
+};
+
+export default MovieReviews;
