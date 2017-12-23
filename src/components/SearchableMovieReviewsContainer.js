4278a449004560b192db92763f4f02d0b759bfff
github username <github email address>
2017-12-23 14:25:11 -0600

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,27 @@
 // Code SearchableMovieReviewsContainer Here
+import React from 'react';
+import MovieReviews from './MovieReviews'
+
+class SearchableMovieReviewsContainer extends React.Component {
+
+  constructor() {
+
+    super();
+
+    this.state = {
+      reviews: [],
+      searchTerm: ""
+    }
+  }
+
+  render(){
+    return (
+      <div className="searchable-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+
+}
+
+export default SearchableMovieReviewsContainer;
