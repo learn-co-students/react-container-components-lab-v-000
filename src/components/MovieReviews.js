094df404cd2de94e6211aaba39f9e1ead6f33fe6
Jim Stricker <jmstricker93@gmail.com>
2017-12-30 13:14:57 -0500

    got some static constants stubbed out for MovieReviews

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
 // Code MovieReviews Here
+
+const Review = ({headline, summary, url}) => {
+  return(
+    <div className='review'>
+      <a href={url}><h3>{headline}</h3></a>
+      <p>{summary}</p>
+    </div>
+  );
+}
+
+const MovieReviews = ({reviews}) => {
+  return(
+    <div className='review-list'>
+      {reviews.map(review => <Review headline="review.headline" summary="review.summary" url="review.url" />)}
+    </div>
+  );
+}
