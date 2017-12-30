4d6dbad9c81819485c64599271a983c84796ab5d
Jim Stricker <jmstricker93@gmail.com>
2017-12-30 14:15:13 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,4 +1,4 @@
-// Code MovieReviews Here
+import React from "react";
 
 const Review = ({headline, summary, url}) => {
   return(
@@ -9,10 +9,14 @@ const Review = ({headline, summary, url}) => {
   );
 }
 
-const MovieReviews = ({reviews}) => {
-  return(
-    <div className='review-list'>
-      {reviews.map(review => <Review headline="review.headline" summary="review.summary" url="review.url" />)}
-    </div>
-  );
+class MovieReviews extends React.Component{
+  render(){
+    return(
+      <div className='review-list'>
+        {this.props.reviews.map(review => <Review headline="review.headline" summary="review.summary" url="review.url" />)}
+      </div>
+    );
+  }
 }
+
+export default MovieReviews;
