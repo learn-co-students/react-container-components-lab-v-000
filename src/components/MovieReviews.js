dec301a6d5227c43e03df7058decbb20174af27a
NickisKnowledge <lifechangingstuff@gmail.com>
2017-12-31 03:46:31 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,27 @@
+import React, { Component } from 'react';
+
 // Code MovieReviews Here
+
+const Review = ({ headline, link, critics_pick }) => {
+  return (
+    <div className='review'>
+      <h3>{headline}</h3>
+      <ul>
+        <li><a href={link.url}>Link</a></li>
+        <li>Critics pick: {critics_pick}</li>
+      </ul>
+    </div>
+  );
+};
+
+const MovieReviews = ({reviews}) => {
+  return (
+    <div className='review-list'>
+      {reviews.map(Review)}
+    </div>
+  );
+};
+
+MovieReviews.defaultProps = { reviews: [] };
+
+export default MovieReviews;
