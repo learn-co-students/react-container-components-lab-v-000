3064e9c44e63704ed0fff67efccb2e3e39267f05
github username <github email address>
2018-01-31 23:00:33 +0000

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -5,7 +5,7 @@ import MovieReviews from './MovieReviews'
 const NYT_API_KEY = 'eb7bccfc45bb46d7a1671aaf80e279ac';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
-
+            https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=eb7bccfc45bb46d7a1671aaf80e279ac
 class LatestMovieReviewsContainer extends React.Component {
     constructor() {
         super();
