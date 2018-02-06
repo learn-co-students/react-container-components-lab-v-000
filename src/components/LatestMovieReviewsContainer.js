7f8a3e9fab63e9b82164696d3442a791764d0fab
Dembe Koi <stephanos@goldmail.etsu.edu>
2018-02-05 19:33:00 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MovieReview from './MovieReviews';
 import 'isomorphic-fetch';
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
@@ -6,3 +7,23 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviews: []
+    };
+  }
+
+
+  render() {
+    
+    return (
+      <div className="latest-movie-reviews"><MovieReview reviews={this.state.reviews} /></div>
+    );
+  }
+}
+
+export default LatestMovieReviewsContainer;
