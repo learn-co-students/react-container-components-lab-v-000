785a0c497825a57495f06109b4eeeaa05aec15ec
emorgan05 <emorgan05@gmail.com>
2018-02-16 16:19:14 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,28 @@
 // Code MovieReviews Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MovieReviews = (reviews) => {
+  return (
+  <div className="review-list">
+    {reviews.reviews.map(review => (
+      <div className="review">
+        <h3>{review.display_title}</h3>
+        <p>{review.summary_short}</p>
+        <p>{review.critics_pick}</p>
+      </div>
+    ))}
+  </div>
+  )
+}
+
+
+MovieReviews.defaultProps = {
+  reviews: [{
+    display_title: "Western",
+    summary_short: "Valeska Grisebach’s film evokes a classic American genre to tease out the contradictions of modern European capitalism.",
+    critics_pick: 1,
+  }]
+};
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,36 @@
 // Code SearchableMovieReviewsContainer Here
+import React from 'react';
+import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json';
+
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      reviews: [],
+      searchTerm: "Romance",
+    };
+  }
+
+  componentDidMount() {
+    let searchURL = URL + `?api-key=${NYT_API_KEY}&query=` + this.state.searchTerm;
+    console.log(searchURL);
+    fetch(searchURL)
+      .then(response => response.json())
+      .then(reviews => this.setState({ reviews: {reviews} }));
+  }
+
+  render () {
+    return (
+      <div className="searchable-movie-reviews">
+        <MovieReviews reviews={this.state.reviews.results} />
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer;
