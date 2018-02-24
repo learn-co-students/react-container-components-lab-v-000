c060cbebfbaf3f8a86383662d04de77d1fa17671
Jonathan Peter Stone <github email address>
2018-02-24 03:53:19 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,26 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({ reviews }) => {
+
+  const reviewList = reviews.map((each_review, index) => {
+    return(
+      <div className ="review" key={each_review.link.url}>
+        <h2>{each_review.display_title}</h2>
+          <p>{each_review.summary_short}</p>
+      </div>
+    )
+  })
+
+  return (
+    <div className="review-list">
+      {reviewList}
+    </div>
+  )
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,56 @@
 // Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import MovieReviews from './MovieReviews';
+import 'isomorphic-fetch';
+
+const API_KEY = 'f98593a095b44546bf4073744b540da0';
+const Url = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+
+
+class SearchableMovieReviewsContainer extends Component {
+
+  constructor() {
+   super();
+
+   this.state = {
+     reviews:[],
+     searchTerm: ''
+   };
+  }
+
+  onSearchChange = e => {
+    this.setState({ searchTerm: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    fetch(Url + `api-key=${API_KEY}` + "&query=" + (this.state.searchTerm))
+
+    .then(response => {
+      if (response.status >= 400) {
+        throw new Error("Bad response from server");
+		  }
+  		return response.json();
+  	})
+  	.then((response) => {
+      this.setState({
+        reviews: response.results
+      })
+    })
+  }
+
+
+render() {
+  return(
+    <div className= "searchable-movie-reviews">
+      <MovieReviews reviews={this.state.reviews}/>
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.onSearchChange} ></input>
+        <button type="submit"> Submit </button>
+      </form>
+    </div>
+    )
+  }
+}
+export default SearchableMovieReviewsContainer
