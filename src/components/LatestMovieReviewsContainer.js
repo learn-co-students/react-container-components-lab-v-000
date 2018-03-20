b9646c26a14df13cebbc7d837fc5673ed896f0b4
JingruZhang <zhang.jingru.93@gmail.com>
2018-03-20 00:30:25 -0700

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+	constructor() {
+		super();
+		this.state = {reviews: []};
+
+	}
+
+	getReviews = () => {
+		fetch(URL)
+		.then(res => res.json())
+		.then(res => this.setState({reviews: res.results}))
+	}
+
+	render() {
+
+		return(
+			<div className="latest-movie-reviews">
+				<button onClick={this.getReviews}>Get movie reviews from NYTimes!</button>
+				<MovieReviews reviews={this.state.reviews} />
+			</div>
+		)
+	}
+}
+
+export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,43 @@
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+            + `api-key=${NYT_API_KEY}`;
+
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+	constructor() {
+		super();
+		this.state = {
+			reviews: [],
+			searchTerm: ""
+		}
+	}
+
+	getLatest = (event) => {
+		this.setState({ searchTerm: event.target.value })
+		this.updateLatest();
+	}
+
+	updateLatest() {
+		const latestUrl = URL + `&query=${this.state.searchTerm}`
+		fetch(latestUrl)
+		.then(res => res.json())
+		.then(res => this.setState({reviews: res.results}))
+	}
+
+	render() {
+		return(
+			<div className="searchable-movie-reviews">
+				<input 
+				value={this.state.searchTerm}
+				onChange={this.getLatest} />
+				<MovieReviews reviews={this.state.reviews} />
+			</div>
+		)
+	}
+}
+
+export default SearchableMovieReviewsContainer;
