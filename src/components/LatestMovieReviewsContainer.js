14b579a9dc408a4d4f4cce58828bcd7745991cba
jss530 <jsifre01@gmail.com>
2018-03-22 13:12:06 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const NYT_API_KEY = '';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentWillMount() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(response => this.setState
+        ({ reviews: response.results })
+      );
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <h1> NYT Latest Reviews: </h1>
+        <MovieReviews reviews = {this.state.reviews}/>
+      </div>
+    )
+  }
+}
+
+export default LatestMovieReviewsContainer;
+
+
+//
+// The LatestMovieReviewsContainer should have a top-level wrapping element with class latest-movie-reviews.
+// The two container components you've been asked to create will use this single presentational component
+// in different ways.
+// The first, <LatestMovieReviewsContainer>, will fetch a list of the most recent reviews and display them
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,24 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const Review = ({ title, summary_short }) => {
+  return (
+    <div className="review">
+
+      <h3>{title}</h3>
+      <p>{summary_short}</p>
+      <br/>
+    </div>
+  )
+}
+
+const MovieReviews = ({ reviews }) =>
+  <div className="review-list">
+    {reviews.map(Review)}
+  </div>
+
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
