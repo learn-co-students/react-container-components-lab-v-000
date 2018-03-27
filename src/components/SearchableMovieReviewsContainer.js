5e05b31e1b264dee3a1fdf18ccf90968c4d1101c
github username <github email address>
2018-03-27 13:21:58 -0400

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,30 @@
 // Code SearchableMovieReviewsContainer Here
+import React from 'react';
+
+import MovieReviews from './MovieReviews';
+
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+
+
+    this.state = {
+      reviews: [],
+      searchTerm: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('api.nytimes.com/svc/movies/v2/reviews/search.json?query=<search-term>')
+    .then(res => res.json())
+    .then(reviews => this.setState({reviews}))
+  }
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+      <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
+export default SearchableMovieReviewsContainer
