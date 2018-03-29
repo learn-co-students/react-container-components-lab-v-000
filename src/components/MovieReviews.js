8a3a742773c978ac63c970990544851065c8b44a
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-29 12:57:36 +0200

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
-// Code MovieReviews Here
+import React, { Component } from 'react';
+
+const singleReview = (reviews) => {
+  if (reviews.length > 0) {
+    return reviews.map((review, i) => <li key={i} className="review">{review.display_title}</li>)
+  }
+}
+
+const MovieReviews = ({ reviews }) => {
+    return (
+      <ol className="review-list">
+        {singleReview(reviews)}
+      </ol>
+    )
+}
+
+MovieReviews.defaultProps = { reviews: [] }
+
+export default MovieReviews
