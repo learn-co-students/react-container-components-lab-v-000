471d3245a6acb6bb4f92edeec0b8ce5ffcad54f2
Christian Egglin <cegglin@conncoll.edu>
2018-04-05 18:37:37 +0000

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,46 @@
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = `https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=${NYT_API_KEY}&query=`;
+
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchTerm: '',
+      reviews: []
+    };
+  }
+
+  handleSearchInputChange = (event) => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(URL + this.state.searchTerm)
+      .then(response => response.json())
+      .then(data => this.setState({ reviews: data.results }))
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="search-input">Search NYT Reviews</label>
+          <input type="text" id="search-input" onChange={this.handleSearchInputChange} />
+          <button type="submit">Search</button>
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer;
