aba00b80dc4b6f834ed2acab782d0162a2d1f842
David Towster <djt4xe@virginia.edu>
2018-04-23 13:03:15 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import testReviews from './test-reviews';
+import MovieReviews from './components/MovieReviews.js';	
 
 import LatestMovieReviewsContainer from './components/LatestMovieReviewsContainer';
 import SearchableMovieReviewsContainer from './components/SearchableMovieReviewsContainer';
 
 ReactDOM.render(
   <div className="app">
-    <SearchableMovieReviewsContainer />
-    <LatestMovieReviewsContainer />
+    <MovieReviews reviews={testReviews} />
   </div>,
   document.getElementById('root')
 );
