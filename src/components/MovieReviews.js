86adcf6aa355c8c6cfa0c21d1649f105c44a25ba
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-04-25 03:19:38 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,23 @@
 // Code MovieReviews Here
+import React from 'react';
+
+class MovieReviews extends React.Component {
+
+}
+
+export default MovieReviews;
+
+// const Book = ({ title, img_url }) => {
+//   return (
+//     <div className="book">
+//       <img src={img_url} />
+//       <h3>{title}</h3>
+//     </div>
+//   )
+// }
+ 
+// const BookList = ({ books }) => (
+//   <div className="book-list">
+//     {books.map(book => <Book title="book.title" img_url="book.image_url" />)}
+//   </div>
+// );
