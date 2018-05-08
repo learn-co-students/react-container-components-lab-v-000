bf69305c5703ffce27483560639ae4e345386f00
jennianelson <jennifernelson118@gmail.com>
2018-05-08 14:19:06 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MovieReviews from './MovieReviews'
 import 'isomorphic-fetch';
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
@@ -6,3 +7,31 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+// fetch a list of the most recent reviews and display them
+// should have a top-level wrapping element 
+//    with class latest-movie-reviews.
+
+export default class LatestMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      reviews: [],
+    };
+  }
+
+  componentWillMount() {
+    fetch(URL)
+      .then(res => res.json())
+      .then((response => this.setState({ reviews: response.results })));
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <h3>The Latest Reviews:</h3>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,58 @@
 // Code SearchableMovieReviewsContainer Here
+// provide a searchable interface allowing the user to enter a search term
+// and then receive a list of reviews that match the search term(s)
+// should have a top-level wrapping element 
+//   with class searchable-movie-reviews.
+
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?' + `api-key=${NYT_API_KEY}&query=`;
+
+export default class LatestMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        reviews: [],
+        searchTerm: ""
+    }
+  }
+
+  searchReviews = (event) => {
+    this.setState({
+      searchTerm: event.target.value,
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    
+    fetch(URL + this.state.searchTerm)
+      .then(res => res.json())
+      .then((response => this.setState({reviews: response.results})))
+  }
+
+  displayResults = () => {
+    const {reviews} = this.state
+    if (reviews.length > 0) {
+      return <h3>Search Results:</h3>
+    }
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          Search: <input onChange={this.searchReviews}></input>
+          <button type="submit">Submit</button>
+        </form>
+        {this.displayResults()}
+          
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
