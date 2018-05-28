dd6ee034dc6fd06cba27f1310b2ee1d2a638c01a
Vicky Lau <vlau.nyc@gmail.com>
2018-05-28 02:57:29 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,26 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const Review = ({
+  headline,
+  byline,
+  publication_date,
+  summary_short,
+  link
+}) => {
+  return (
+    <div key={headline} className="review">
+      <h2><a href={link.url}>{headline}</a></h2>
+      <h5>By: {byline},  published on {publication_date}</h5>
+      <p>Summary: {summary_short}</p>
+      <hr/>
+    </div>
+  )
+};
+
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,45 @@
-// Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+
+import MovieReviews from './MovieReviews';
+
+const NYT_API_KEY = 'cab46dd034b54850b2ad0bfb91bfed47';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+           + `api-key=${NYT_API_KEY}&query=`;
+
+class SearchableMovieReviewsContainer extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      reviews: [],
+      searchTerm: ''
+    };
+  }
+
+  handleSearchInputChange = event => this.setState({ searchTerm: event.target.value });
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    fetch(URL.concat(this.state.searchTerm))
+      .then(response => response.json())
+      .then(response => this.setState({ reviews: response.results }));
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleSearchInputChange} type="text" />
+          <button type="submit">Submit</button>
+        </form>
+        {this.state.reviews.length > 0 && <h1>Movie Review By Search:</h1>}
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
+
+export default SearchableMovieReviewsContainer;
