9004a06c53692fe08fb5b05357dd074fe18b5a7f
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-05 16:10:59 -0400

    All tests passing except under SearchableMovieReviews...
    ...some interesting warnings for other tests, but still, seemingly passing.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,3 +6,45 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+import MovieReviews from './MovieReviews'
+
+class LatestMovieReviewsContainer extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      reviews: []
+    }
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return (this.state.reviews !== nextState.reviews)
+  // }
+
+  // Not sure why we don't use componentDidMount since
+  // componentWillMount is problematic
+
+  componentWillMount() {
+    fetch(URL)
+    .then((res) => {
+      return res.json()
+    })
+    .then((res) => {
+      this.setState({
+        reviews: res.results
+      })
+    })
+
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <h2>The Latest Reviews:</h2>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
+
+export default LatestMovieReviewsContainer
