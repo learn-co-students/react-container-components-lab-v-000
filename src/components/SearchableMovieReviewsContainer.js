6080d67e31207898987aee9c84fccea51258d24b
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-05 16:20:02 -0400

    All tests passing but last one

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,27 @@
 // Code SearchableMovieReviewsContainer Here
+
+import React from 'react'
+
+import MovieReviews from './MovieReviews'
+
+class SearchableMovieReviewsContainer extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      reviews: [],
+      searchTerm: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+      </div>
+    )
+  }
+
+}
+
+
+export default SearchableMovieReviewsContainer
