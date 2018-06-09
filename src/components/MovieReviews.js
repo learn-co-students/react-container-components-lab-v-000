611f91084843dad74e10827f88626fea385651cc
Hoon Lee <yl.hoony@gmail.com>
2018-06-08 17:38:30 -0700

    done

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MovieReviews = ({reviews}) => {
+  return (
+    <div className="review-list">
+      {reviews.map(review => <p className="review">{review.display_title}</p>)}
+    </div>
+  )
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews;
