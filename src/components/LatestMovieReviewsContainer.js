537ec632bb01eb4fdbe9122c29cb0998fef50b4b
remclay <remmy.lauren@gmail.com>
2018-06-16 13:38:27 +1000

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews';
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const NYT_API_KEY = '7a608b45df6043668bf426791ba211c4';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(response => response.json())
+    .then(response => this.setState({reviews: response.results}))
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <h3>Latest Reviews</h3>
+        < MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,47 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const Review = ({title, critics_pick, headline, summary_short}) => {
+  return (
+    <div className="review">
+      <h3>{title} - {critics_pick}</h3>
+      <h4>{headline}</h4>
+      <p>{summary_short}</p>
+    </div>
+  )
+}
+
+const MovieReviews = ({ reviews }) => (
+  <div className="review-list">
+    {reviews.map(Review)}
+  </div>
+)
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews
+
+// export default class MovieReviews extends React.Component {
+//   constructor(props) {
+//     super(props)
+//   }
+//
+//   render() {
+//     return (
+//       <div className="review-list">
+//           {this.props.reviews.map(review =>
+//             <div className="review" id={review.title}>
+//               <h3>{review.title} - {review.critics_pick}</h3>
+//               <h4>{review.headline}</h4>
+//               <p>{review.summary}</p>
+//             </div>
+//           )}
+//       </div>
+//     )
+//   }
+// }
+//
+// MovieReviews.defaultProps = {
+//   reviews: []
+// };
