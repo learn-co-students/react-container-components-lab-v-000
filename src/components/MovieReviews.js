886e5e38808f2e03730d906e3791735f45653351
scliff6262 <sclifford6262@gmail.com>
2018-07-07 16:02:21 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+  this.listMovies = () => {
+    console.log(reviews)
+    return reviews.map(function(movieObj){
+        return (
+        <div className="review">
+          <h3>{movieObj.display_title}</h3>
+          <p>Critic Score: {movieObj.critics_pick}</p>
+          <p><a href={movieObj.link.url}>{movieObj.link.suggested_link_text}</a></p>
+        </div>
+      )
+    })
+  }
+
+
+  return(
+    <div className="review-list">
+      {this.listMovies()}
+    </div>
+  )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,16 @@
-// Code SearchableMovieReviewsContainer Here
+import React from 'react'
+
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return(
+      <div className="searchable-movie-reviews"></div>
+    )
+  }
+}
+
+
+export default SearchableMovieReviewsContainer
