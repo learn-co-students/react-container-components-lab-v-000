a8292e3f1da9cf23df310eb910d47673e2a0b980
heislercreative <brennan@heislercreative.com>
2018-07-15 16:14:35 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import SearchableMovieReviewsContainer from './components/SearchableMovieReviews
 ReactDOM.render(
   <div className="app">
     <SearchableMovieReviewsContainer />
+    <hr />
     <LatestMovieReviewsContainer />
   </div>,
   document.getElementById('root')
