3dbc419ab59084b010cc7805bdd7442a5821626b
Randall Robinson <keith_adams007@yahoo.com>
2018-07-30 17:24:07 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
 // Code MovieReviews Here
+import React from 'react'
+
+
+class MovieReviews extends React.Component {
+
+  render() {
+    return this.props.reviews.map(review =>{
+      return (
+        <div className="review-list" key={review.index}>
+        {review.text}
+        </div>
+      )
+    })
+  }
+
+}
+
+
+
+export default MovieReviews
