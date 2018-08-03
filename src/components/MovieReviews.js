6a871027f6953285d07a9f227454450aff0143c7
Jirles <emily.jirles@gmail.com>
2018-08-03 13:42:54 -0400

    Passing tests, browser gets blocked by CORS

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = (props) => {
+    return(
+        <div className="review-list">
+            {props.reviews.map(review => { 
+                return (
+                    <div className='review' key={review.display_title}>
+                        <strong><a href={review.link.url}>{review.headline}</a></strong>
+                        <small> Critic's Pick: {review.critics_pick ? 'Y' : "N"}</small>
+                        <p>By: {review.byline}</p>
+                    </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,40 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            reviews: [],
+            searchTerm: ''
+        }
+    }
+
+    fetchQueryResults = (e) => {
+        e.preventDefault();
+        fetch(URL + `?query=${this.state.searchTerm}`)
+            .then(resp => resp.json())
+            .then(json => this.setState({reviews: json.results, searchTerm: this.state.searchTerm}));
+    }
+
+    handleChange = (e) => {
+        this.setState({searchTerm: e.target.value});
+    }
+
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+                <h3>Searchable Movie Reviews</h3>
+                <form onSubmit={this.fetchQueryResults}>
+                    <input type='text' id='query' value={this.state.searchTerm} onChange={this.handleChange}/>
+                    <input type='submit' value='Search' />
+                </form>
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
