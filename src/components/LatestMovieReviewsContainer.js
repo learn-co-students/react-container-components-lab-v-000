99e40841bb6f6df2bbc7fa4ff01f5b8b6f30c3f1
Lauren Perchitti <laurenperchitti@gmail.com>
2018-08-05 18:21:45 +0000

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,26 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends React.Component {
+	constructor(){
+		super();
+
+		this.state = {
+			reviews: []
+		};
+	}
+
+   componentWillMount() {
+     fetch(URL)
+       .then(response => response.json())
+       .then(reviews => this.setState({ reviews: reviews.results }))
+   }
+
+	render() {
+		return (
+			<div className="latest-movie-reviews">
+				<MovieReviews reviews={this.state.reviews} />
+			</div>
+		);
+	}
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,35 @@
+// Code SearchableMovieReviewsContainer Here
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${NYT_API_KEY}&query=`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchTerm: '',
+      reviews: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(URL + this.state.searchTerm)
+      .then(response => response.json())
+      .then((reviews => this.setState({ reviews: reviews.results })));
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer;
