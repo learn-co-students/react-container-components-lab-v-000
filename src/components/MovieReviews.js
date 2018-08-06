897ff9421c881b28f5e5dc39117751004eff0f36
ArielFeingold <feingold.ariel@gmail.com>
2018-08-06 16:00:58 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,15 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReview = ({reviews}) => (
+  <div className="review-list">
+    {reviews.map((review, idx) =>
+      <div className="review" key={idx}>
+        <h3>Movie Title: {review.display_title}</h3>
+        <p>Description: {review.summary_short}</p>
+        <spen>Release Date: {review.opening_date}</spen>
+        <h5>MPAA Rating: {review.mpaa_rating}</h5>
+    </div>)}
+  </div>
+)
+
+export default MovieReview;
