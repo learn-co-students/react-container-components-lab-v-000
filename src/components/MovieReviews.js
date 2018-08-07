7aebaec778edb7e929be5f901e8b07110d6174e7
lizbur10 <liz.burton147@gmail.com>
2018-08-07 16:12:33 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({ reviews }) => (
+    <div className="review-list">
+        {reviews.map(review => 
+            <div className="review">
+                <h3>{ review.display_title }</h3>
+                <p>{ review.summary_short }</p>
+            </div>
+        )}
+    </div>
+)
+
+MovieReviews.defaultProps = {
+    reviews: []
+  }
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,31 @@
 // Code SearchableMovieReviewsContainer Here
+import React, { Component } from 'react';
+import MovieReviews from './MovieReviews';
+
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=<search-term>'
+            + `api-key=${NYT_API_KEY}`;
+
+export default class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+        super()
+        this.state = { 
+            searchTerm: "",
+            reviews: [] 
+        }
+    }
+
+    componentDidMount() {
+        fetch(URL)
+            .then(response => response.json())
+            .then(reviews => this.setState({ reviews: reviews }))
+    }
+
+    render() {
+        return(
+            <div className="searchable-movie-reviews">
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
