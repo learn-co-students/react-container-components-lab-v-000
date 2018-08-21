e010e00cfc2c2593d62fcff14cd8eb6c7474413e
Raiet <raietsaeed@gmail.com>
2018-08-21 03:32:55 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,10 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = (props) =>
+  <div className="review-list">
+    {props.reviews.map(review => <a className="review" href={review.link.url}>{review.display_title}</a>)}
+  </div>;
+
+
+export default MovieReviews;
