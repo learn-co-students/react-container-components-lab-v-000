4ecbe7ce98d3369de91384e9bc37c866e1dc0565
badlychadly <chadmontoya21@yahoo.com>
2018-09-06 20:21:54 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,27 @@
-// Code MovieReviews Here
+
+import React from 'react'
+
+const MovieReviews = ({reviews}) => {
+    // debugger;
+
+    const reviewLayout = reviews => {
+        return reviews.map(({byline, headline, summary_short}) => {
+            return (<div key={headline} className="review">
+                <h2>{headline}</h2>
+                <span><strong>Author:</strong> {byline}</span>
+                <br/>
+                <h4 htmlFor="">Summary:</h4>
+                <blockquote>{summary_short}</blockquote>
+                <br/><br/>
+            </div>)
+
+        })
+    }
+
+
+    return (
+        <div className="review-list">{reviewLayout(reviews)}</div>
+    )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,49 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    state = {
+        reviews: [], 
+        searchTerm: ""
+    }
+
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.searchTerm} onChange={this.handleChange}/>
+                    <input type="submit" value="Search Movies"/>
+                </form>
+            </div>
+        )
+    }
+
+    // fetchMovies = () => {
+    //     // debugger;
+    //     // event.preventDefault()
+    //     fetch(`${URL}&query=${this.state.searchTerm}`)
+    //     .then(resp => resp.json())
+    //     .then(results => {
+    //         debugger;
+    //     })
+    // }
+
+    
+
+    handleSubmit = event => {
+        event.preventDefault()
+        fetch(`${URL}&query=${this.state.searchTerm}`)
+        .then(resp => resp.json())
+        .then(results => {
+            debugger;
+        })
+    }
+
+
+    handleChange = event => {
+        this.setState({searchTerm: event.target.value})
+    }
+}
