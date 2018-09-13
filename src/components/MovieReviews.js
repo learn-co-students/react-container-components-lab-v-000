af8f27c9e4fc4c651d9f9999e6f348964aed46b5
Kiveo <mavericktesini@gmail.com>
2018-09-12 18:17:27 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieReviews = (props) => {
   return (
     <div className={'review-list'} >
-      {props.reviews && props.reviews.length > 0 ? props.reviews.map((review, index) => <p key={index} className={'review'}><h3>{review.headline}</h3> {review.summary_short}</p>) : 'blank'}
+      {props.reviews && props.reviews.length > 0 ? props.reviews.map((review, index) => <article key={index} className={'review'}><h3>{review.headline}</h3> {review.summary_short}</article>) : 'blank'}
     </div>
   );
 }
