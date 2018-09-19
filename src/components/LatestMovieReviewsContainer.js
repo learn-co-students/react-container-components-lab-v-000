99623575a81214b17a1b5c92b6701703f18c0834
nptravis <nptravis@yahoo.com>
2018-09-19 13:57:06 +0700

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -3,7 +3,47 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const MY_API_KEY = 'cc09868df44044c0861103cfdeffc16c';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${MY_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+	
+	constructor(){
+		super()
+		this.state = {
+			reviews: []
+		}
+	}
+
+	updateState = (data) => {
+		this.setState({
+			reviews: data
+		})
+	}
+
+	componentDidMount(){
+		fetch(URL)
+			.then(response => {
+				if (response.status >= 400) {
+					throw new Error("Bad response from server");
+				}
+				return response.json();
+			})
+			.then(stories => {
+				// console.log(stories)
+				this.updateState(stories);
+			});
+	}
+
+	render() {
+		return (
+			<div className="latest-movie-reviews">
+				<MovieReviews reviews={this.state.reviews}/>
+			</div>
+		)
+	}
+}
+
+export default LatestMovieReviewsContainer
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,57 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const MY_API_KEY = 'cc09868df44044c0861103cfdeffc16c';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+            + `api-key=${MY_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+	constructor(){
+		super()
+		this.state = {
+			searchTerm: "",
+			reviews: []
+		}
+	}
+
+	handleChange = (event) => {
+		this.setState({
+			searchTerm: event.target.value
+		})
+	}
+
+	updateState = (stories) => {
+		this.setState({
+			reviews: stories
+		})
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault()
+		fetch(URL +"?query=" + this.state.searchTerm)
+			.then(response => {
+				if (response.status >= 400) {
+					throw new Error("Bad response from server");
+				}
+				return response.json();
+			})
+			.then(stories => {
+				this.updateState(stories);
+			});
+	}
+
+	render() {
+		return (
+			<div className="searchable-movie-reviews">
+				<form onSubmit={event => this.handleSubmit(event)}>
+					<input type="text" onChange={event => this.handleChange(event)} />
+					<input type="submit" />
+				</form>
+				< MovieReviews reviews={this.state.reviews} />
+			</div>
+		)
+	}
+}
+
+export default SearchableMovieReviewsContainer
