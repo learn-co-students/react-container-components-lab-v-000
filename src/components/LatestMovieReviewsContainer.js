32c8ea0e5fa0e850a7c06c614d8b15776cbb7a54
github username <purnima.nyc@gmail.com>
2018-09-24 22:17:24 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,33 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component{
+  constructor(){
+    super()
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(URL)
+    .then(function(resp){
+      if (resp.status >= 404){
+        throw new Error('Something went wrong')
+    }
+      resp.json()
+    }).then(reviews => this.setState({reviews}))
+  }
+
+  render(){
+    return(
+      <div className='latest-movie-reviews'>
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+
+}
+
+
+export default LatestMovieReviewsContainer;
