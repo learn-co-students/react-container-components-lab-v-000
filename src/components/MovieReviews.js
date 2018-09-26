21bf06e9b655c6e8c98d9896d1a9b0758e7d6aa0
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-09-26 12:05:58 -0700

    init commit

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = props => {
+  return (
+    <div className="review-list">
+      {props.reviews.map(review => {
+        return <p className="review">{review.display_title}</p>;
+      })}
+    </div>
+  );
+};
+
+export default MovieReviews;
