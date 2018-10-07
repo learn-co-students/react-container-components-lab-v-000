0638add6be5d9651f8d47db95b6ed622ff4e6f3b
nmlstyl <joey.michalina.mariano@gmail.com>
2018-10-07 04:10:04 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,23 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({reviews}) => {
+
+  return <div className="review-list">{ createListItems(reviews) }</div>
+
+}
+
+
+
+function createListItems(reviews){
+  return reviews.map( (r) => <div className='review'><ul>
+                                <li>{r.display_title}</li>
+                                <li>{r.byline}</li>
+                                <li>{r.link.url}</li>
+                              </ul></div>  )
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,49 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      reviews: [],
+      searchTerm: ''
+    };
+  }
+
+  handleClick(term, event) {
+    event.preventDefault()
+    fetch(URL + `?query=` + term)
+      .then(res => res.json())
+      .then(resp => {
+        let rev = resp.results
+        this.setState({ reviews: rev })
+      })
+  }
+
+  handleTyping = (e) => {
+    debugger
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+
+
+  render() {
+    return(
+      <div className='searchable-movie-reviews'>
+      <form onSubmit={ (event) => this.handleClick(this.state.searchTerm, event) }>
+        <input type="text" value={ this.state.searchTerm } onChange={ (event) => this.handleTyping(event) }></input>
+        <input type="submit" value="Submit"></input>
+      </form>
+      <MovieReviews reviews={ this.state.reviews }/>
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer
