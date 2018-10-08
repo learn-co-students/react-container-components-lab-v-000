3e6af63acd8db1a6b2f8cb6c91f30eac2d236735
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-10-08 18:40:56 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,23 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+
+  state = {
+    reviews: []
+  }
+
+  componentDidMount() {
+    fetch(URL).then(resp => resp.json()).then(reviews => this.setState({reviews: reviews.results}))
+  }
+
+
+  render(){
+    return <div className="latest-movie-reviews"><MovieReviews reviews={this.state.reviews} text="Latest Movie Reviews"/></div>
+  }
+
+
+}
+
+export default LatestMovieReviewsContainer
