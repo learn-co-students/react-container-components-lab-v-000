d97b74871e1c30e02c32fff98e99976c5e4324ff
Kim Gonzales <Kimgo589@gmail.com>
2018-10-20 23:06:50 -0400

    pass moviereviews componenent tests

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+const Review = ({headline}) => { return<div className="review"><p>{ headline }</p> </div>}
 
+// const MovieReviews = ({ reviews }) =>{
+//     <div className="review-list">
+//       { reviews.map(review => <Review  key={review.url} headline={review.headline}/>)}
+//     </div>
+// }
 
-const MovieReviews = (props) =>{
-  return <div className="review-list"> HI</div>
-}
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
 
 //Which component should iterate over all the of the movie reviews?
 export default MovieReviews
