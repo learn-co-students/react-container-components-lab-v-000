5d04812ae5ac689c248e8743ab9029014e2cdfc0
Dillorom <dabdulloeva@gmail.com>
2018-10-26 10:00:53 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,8 @@
 // Code MovieReviews Here
+import React from 'react'
+const MovieReviews = props => 
+<div className="review-list">
+    <li className="revew"></li>
+</div>
+
+export default MovieReviews;
