313396efd7b982f372e58dac3bc18c094de69944
tsyaeger <tsyaeger@gmail.com>
2018-11-18 23:25:00 -0800

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = (props) => {
+
+	return (
+		<ul class='review-list'>
+			{props.reviews.map(review => (
+				<i class='review'>
+					<h3>{review.headline}</h3>
+				</i>
+			))}
+		</ul>
+	)
+}
+
+ export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,6 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+
+const queryReviews = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${searchTerm}'
