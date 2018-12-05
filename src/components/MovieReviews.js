2e7cebe4d8964907bc22c29398a939b8d05e6a03
allisonkadel <owner@allisonkadel.com>
2018-12-05 06:43:35 -0800

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,12 +1,19 @@
 // Code MovieReviews Here
 import React from 'react'
 
-const MovieReviews = ({reviews}) => 
-    <ul className='review-list'>
-        {reviews.map(review => {
-            return <li key={review.headline} className='review'>{review.display_title}-{review.byline}-{review.summary_short}</li>
-        })}
-        
-    </ul>
+const MovieReviews = ({reviews}) => {
+    return (
+        <section className='review-list'>
+            {reviews.map(review => (
+                <ul key={review.headline} className='review'>
+                    <h2>{review.display_title}</h2>
+                    <li>{review.byline}</li>
+                    <li>{review.summary_short}</li>
+                </ul>
+            ))}
+            
+        </section>
+    )
+}
 
 export default MovieReviews
