374f0a29900ec1272f89eb404771597af4fb3641
Rygel-XVI <micha232@umn.edu>
2018-12-08 16:30:01 -0800

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,29 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = ({
+      reviews: []
+    })
+  }
+
+  componentDidMount() {
+    this.fetchReviews()
+  }
+
+  fetchReviews = () => {
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(json =>{
+      this.setState ({reviews: json.results})
+    })
+  }
+
+  render() {
+    return <div className='latest-movie-reviews'><MovieReviews reviews={this.state.reviews} /></div>
+  }
+}
+
+export default LatestMovieReviewsContainer
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,22 @@
-// Code MovieReviews Here
+// Your MovieReviews component should be stateless and functional.
+//
+// You are free to lay out each review as you like using the data that the API returns, but make sure that the component outputs a top-level element with the class review-list and that each review is wrapped by an element with class review.
+import React from 'react'
+
+const Review = ({ link, display_title, mpaa_rating, summary_short }) => {
+  return (
+
+  <li className='review' key={link.url}>
+  <h3>{display_title} {mpaa_rating !== '' ? ' - \"' + mpaa_rating + '\"': ''}</h3>
+  <p>{summary_short}</p>
+  </li>
+
+)}
+
+const MovieReviews = ({ reviews }) => <div className='review-list'><ul>{reviews.map(Review)}</ul></div>
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews
