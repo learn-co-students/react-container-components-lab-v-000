65e529607bbff3c9fa5a46f515c7028ef25091f9
kakhalghi <kevin.akhlaghi@gmail.com>
2018-12-17 10:35:16 -0800

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,26 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount(){
+        fetch(URL)
+        .then(res => res.json())
+        .then(response => this.setState({ reviews: response.results }));
+    }
+
+    render(){
+        return(
+        <div className="latest-movie-reviews">
+        <h2>Latest Movie Reviews</h2>
+        <MovieReviews reviews={this.state.reviews} />
+        </div>
+        )
+    }
+}
