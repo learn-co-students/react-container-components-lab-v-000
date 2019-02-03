803af5709552297d7bc1c8972f71dcc34aafb014
terryblue99 <terryblue@comcast.net>
2019-02-03 16:37:52 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = props => {
+	// console.log('props length: ', props.reviewList)
+	return (
+	   	<div className="review-list">
+	   		Hello Reviews!   		 		
+	  	</div>
+	)	
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
+import 'es6-promise/auto'
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
+const NYT_API_KEY = 'AoOucKUt3jpmSGw7FgqzD6nHaJ0oCDbz';
+
+class SearchableMovieReviewsContainer extends React.Component {
+  
+	constructor() {
+    super()
+ 
+    this.state = {
+      reviews: [],
+      searchTerm: 'daughter'
+    };
+	}
+
+	componentDidMount() {
+  	const NYT_QUERY = 'daughter'
+	  const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+			+ `query=${NYT_QUERY}`
+			+ '&'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+    fetch(URL)
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      })
+      .then(data => this.setState({ reviews: data.results })) 
+  }
+
+	render() {
+  
+    return (
+       <div className="searchable-movie-reviews">
+        Hello Searchable!
+        <MovieReviews reviewList={this.state.reviews}/>
+      </div>
+    )
+  }
+
+}
+
+export default SearchableMovieReviewsContainer
