40dc76cd0d1808ca3d599f75849f7d720101e348
btmccollum <btmcco8@gmail.com>
2019-02-03 22:03:38 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
 // Code MovieReviews Here
+import React from 'react';
+import shortid from 'shortid';
+
+const Review = (props) => {
+    return (
+        <div key={shortid.generate()} className="review">
+            <h2><a href={props.link.url}>{props.headline}</a></h2>
+            <span className="author">{props.byline}</span>
+            <blockquote>{props.summary_short}</blockquote>
+        </div>
+    )
+}
+
+const MovieReviews = (props) => <div className="review-list">{props.reviews.map(Review)}</div> 
+
+MovieReviews.defaultProps = {
+    reviews: []
+}
+
+export default MovieReviews
