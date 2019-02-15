c24bdc28b9ba38a15cf946e6f458237f621c74ba
Kachel Wittig <kjwittig@gmail.com>
2019-02-15 15:06:10 -0600

    Donezo

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const Review = ({ display_title }) => (
+  <div className="review">
+    return <h5>{display_title}</h5>
+  </div>
+)
+
+const MovieReviews = ({ reviews }) => (
+  <div className="review-list">{reviews.map(Review)}</div>
+)
+
+MovieReviews.defaultProps = {
+  reviews: [],
+}
+
+export default MovieReviews
