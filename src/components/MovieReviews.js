06f563633b60dde4932b0e91f2edf315a1b35162
github username <github email address>
2019-02-18 16:27:27 -0800

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,16 @@
-// Code MovieReviews Here
+import React from 'react';
+
+function MovieReviews(props) {
+  return (
+    <div className="review-list">
+      { props.reviews.map((review,i) =>
+        <li key={i} className='review'>
+          <em><h4><font color='blue'>{review.display_title}</font></h4></em>
+          <small>{review.headline}</small>
+          </li>
+        )}
+    </div>
+  );
+}
+
+export default MovieReviews;
