01d065e886446d8adcaed31497df4e7769d05611
Heidi Engstrom <hengstrom25@gmail.com>
2019-03-04 23:50:59 +0000

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'R6FD3DFnKvPYMVYyVgIOcEewVbT4ArnR';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+            + `api-key=${NYT_API_KEY}&query=`;
 
 // Code SearchableMovieReviewsContainer Here
+
+export default class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+      super()
+      
+      this.state = {
+        reviews: []
+      }
+    }
+    
+    render() {
+      
+    }
+  
+}
