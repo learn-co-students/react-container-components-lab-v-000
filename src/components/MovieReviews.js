fd53c3171db761586a237cf720b983f167e21fce
Yazgylych Rahymov <y.rahymov@gmail.com>
2019-04-08 13:58:50 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = props => {
+  return(
+    <div className="review-list">
+      {props.reviews.map(review => (
+        <div className="review" key={review.headline}>
+          <h2>{review.headline}</h2>
+          <p>{review.summary_short}</p>
+          {review.multimedia !== null ? (<img src={review.multimedia.src} alt={review.display_title}/>) : null }
+        </div>
+    ))}
+   </div>
+  )
+}
+
+export default MovieReviews
