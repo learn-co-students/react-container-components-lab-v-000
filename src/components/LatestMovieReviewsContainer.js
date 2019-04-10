672eb94d6f01add1765b1115e1f841170759a5a8
amckean12 <amckean@me.com>
2019-04-09 20:55:00 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,16 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+  state ={
+    reviews: []
+  }
+
+  render(){
+    return(
+      <div className="latest-movie-reviews">
+      </div>)
+  }
+}
+
+export default LatestMovieReviewsContainer
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,8 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = () => {
+  return(<div className="review-list"></div>);
+}
+
+export default MovieReviews
