b98d3a939232ca26733918601fd3bd9904910335
mattetress <mattetress@me.com>
2019-05-01 02:20:58 -0500

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = '5ypc6iZGL6VfDdNcaoxdxNlAUvCoQnKU';
+const NYT_API_KEY = '';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
