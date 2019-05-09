f54469b23d1c899926c0c8821ff19d1c7d096c08
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2019-05-09 00:12:40 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReviewCard from './ReviewCard'
 
   const MovieReviews = ({reviews}) => {
-    const reviewCards = reviews.map(review => <ReviewCard key={Math.floor(Math.random()* (100))} review={review} myRandomValue={true} />)
+    const reviewCards = reviews.map(review => <ReviewCard key={Math.floor(Math.random()* (10000))} review={review} myRandomValue={true} />)
     return (
       <div className="review-list">
         {reviewCards}
