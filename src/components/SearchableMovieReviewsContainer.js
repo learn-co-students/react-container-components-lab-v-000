f06fb3bc201fffad2b02cce770b6a9a81ec0bd2a
epic0448 <samuelogundiran@Yahoo.com>
2019-05-11 19:20:37 -0400

    Need to Look over this again.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,49 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+
+class  SearchableMovieReviewsContainer extends React.Component{
+    constructor(props) {
+
+        state = {
+            searchTerm: '', reviews: []
+        };
+
+        handleSearchInputChange = event =>{
+                this.setSate({ searchTerm: event.target.value});
+        }
+
+        handleSubmit = event => {event.preventDefalut();
+
+                    fetch(BASE_URL.contact(this.state.searchTerm))
+                    .then(res => res.json())
+                    .then(res => this.setState({ reviews: res.result}));
+                };
+
+
+       render(){
+           return(
+               <div className="searchable-movie-reviews">
+               <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="search-input">Search Movie Reviews</label>
+                    <input
+                     id = "search-input"
+                     type ="text"
+                     style={{width: 300}}
+                     type="text"
+                     style={{ width: 300}}
+                     onChange={this.handleSearchInputChange}
+                    />
+                    <button type="submit">Submit</button>
+               </form>
+                    {typeof this.state.reviews === 'object' && 
+                    this.state.reviews.lenghth > 0 && <h2>Movie Review By Search:</h2>  }
+
+                    <MovieReviews reviews={this.state.reviews} />
+               </div>
+           )
+       }
+
+}
+
+export default SearchableMovieReviewsContainer
