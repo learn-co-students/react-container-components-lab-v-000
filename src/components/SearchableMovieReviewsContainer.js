d6625ae257ae8fefa006167d5b17bbbe3bffc075
Ryan <ryanwevans@icloud.com>
2019-05-21 18:24:58 -0700

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,58 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = 'xhpc2iiwh9zxhJS1KUdMvk2yXHjWXRp3';
+const URL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=${NYT_API_KEY}&query=`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: '',
+            reviews: []
+        }
+    }
+
+    handleInputChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value })
+        
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        fetch(URL.concat(this.state.searchTerm))
+        .then(response => {
+            if (response.status >= 400) {
+                throw new Error("Bad response from server");
+            }
+            return response.json();
+        })
+        .then(data => {
+            // console.log(data.results);
+            this.setState({
+                reviews: data.results
+            }, ()=>{console.log(this.state.reviews)})
+        })
+    }
+
+    render() {
+        return(
+            <div className="searchable-movie-reviews">
+                <form onSubmit={this.handleSubmit}>
+                <label>Search for a Movie Review:
+                    <input type="text" name="searchTerm" value={this.state.searchTerm} onChange={this.handleInputChange} />
+                </label>
+                <button>Search</button>
+                </form>
+                <br/>
+                {this.state.reviews !== '' ?
+                    (<h4>Search Results:</h4> && <MovieReviews reviews={this.state.reviews} />) : null}
+            </div>
+        )
+    }
+
+
+}
+
+export default SearchableMovieReviewsContainer
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 
 import LatestMovieReviewsContainer from './components/LatestMovieReviewsContainer';
 import SearchableMovieReviewsContainer from './components/SearchableMovieReviewsContainer';
+import ErrorBoundary from './components/ErrorBoundary';
 
 ReactDOM.render(
   <div className="app">
+    <ErrorBoundary>
     <SearchableMovieReviewsContainer />
     <LatestMovieReviewsContainer />
+    </ErrorBoundary>
   </div>,
   document.getElementById('root')
 );
