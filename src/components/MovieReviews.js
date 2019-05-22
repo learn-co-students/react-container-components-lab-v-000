e7388c30ce8918dcbc71cb266debdf9db34c7243
Yan C Lin <yonalin1@gmail.com>
2019-05-22 16:46:12 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,27 @@
-// Code MovieReviews Here
+import React, {Component } from 'react';
+import 'isomorphic-fetch';
+
+const MovieReviews = (props) => (
+
+  <div className={'review-list'}>
+    <ul>
+      {props.reviews.map(c => (
+        <div className={'review'}>
+          <li>
+            <div key={c.results}>
+              {c.display_title} - {c.mpaa_rating}
+            </div>
+        </li>
+      </div>
+
+      ))}
+    </ul>
+  </div>
+)
+MovieReviews.defaultProps = {
+  reviews: [],
+}
+
+
+
+export default MovieReviews
