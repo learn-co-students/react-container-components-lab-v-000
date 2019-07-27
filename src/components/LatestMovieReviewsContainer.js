634f61f807be8846367e189b20e585e41b16bd43
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-26 21:07:34 -0400

    Complete LatestMovieReviewsContainer

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,30 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(data => this.setState({
+        reviews: data.results
+      }));
+  }
+
+  render() {
+    return (
+      <div className = 'latest-movie-reviews'>
+        <h3>NYT Latest Movie Reviews</h3>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
+
+export default LatestMovieReviewsContainer;
