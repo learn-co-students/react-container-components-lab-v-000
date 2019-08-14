9736d220e994a34e05c1a6265b37f4a783397140
Kazumi Karbowski <kazkazinus@gmail.com>
2019-08-14 16:07:51 -0400

    nine tests passing

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,40 +1,36 @@
 import React from 'react';
-// import React, { Component } from 'react';
 
-// const MovieReviews = ({reviews}) => {
+const Review = ({
+  headline,
+  byline,
+  link,
+  summary_short
+}) => {
+  return (
 
-//   const formatedReviews = reviews.map(review => review.display_title)
+    <div
+      key={headline}
+      className="review"
+    >
+      <header>
+        <a
+          className="review-link"
+          href={link.url}
+        >
+          {headline}
+        </a>
+        <br/>
+        <span className="author">{byline}</span>
+      </header>
+      <blockquote>{summary_short}</blockquote>
+    </div>
+  );
+};
 
-//   return (
-//     <div className="review-list">
-//       <h3>{formatedReviews}</h3>
-//         {/* <li>{review.display_title}</li>  */}
-//         {/* * <li>critics_pick={review.critics_pick}</li>
-//         <li>summary_short={review.summary_short}</li> */}
-//     </div>
-//  
-const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(review => review.display_title)}</div>;
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
 
 export default MovieReviews;
-
-// class MovieReviews extends Component {
-//   render () {
-//     debugger
-//   // const formatedReviews = this.props.reviews.map(review => {    
-            
-//   // }
-//   //   )
-// console.log(this.props.reviews)
-//    return (
-//      <div className="review-list">
-//        {/* <h3>{formatedReviews}</h3> */}
-//        <h3>Hello</h3>
-
-//          {/* <li>{this.props.review.display_title}</li>  */}
-//          {/* * <li>critics_pick={review.critics_pick}</li>
-//          <li>summary_short={review.summary_short}</li> */}
-//      </div>
-//    );
-//   };
-// }
-//  export default MovieReviews;
