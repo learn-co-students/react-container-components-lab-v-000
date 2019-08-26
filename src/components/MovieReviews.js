9846812bc1639b10ec39bacc145565857977c5e1
Christopher DiStasio <cadistasio@gmail.com>
2019-08-26 06:58:25 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = () => {
+  return (
+    <div className='review-list'>
+      hello
+    </div>
+  )
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,22 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      reviews: [],
+      searchTerm: ''
+    }
+  }
+
+  render(){
+    return(
+      <div className='searchable-movie-reviews'>
+
+      </div>
+    )
+  }
+
+}
