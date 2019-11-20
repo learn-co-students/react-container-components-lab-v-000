77c887a7af55cc8ddda1b4c6635a1f8974fc9e06
github username <github email address>
2019-11-20 17:21:59 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,20 @@
-// Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = ( {reviews} ) => {
+  return (
+    <div className="review-list">
+    <ul>
+    {reviews.map((review, index) =>
+      <div className="review" key={index}>
+        <h3>{review.display_title}</h3>
+        <p>Critics' Pick: {review.critics_pick}</p>
+        <p>By: {review.byline}</p>
+        <p>Summary: {review.summary_short}</p>
+        </div>
+      )}
+      </ul>
+   </div>
+ )
+}
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,43 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+const URL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?`
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      query: "",
+      reviews: []
+    }
+  }
+
+handleSubmit = (event) => {
+  event.preventDefault()
+  fetch(`${URL}` + `&query=${this.state.query}`)
+  .then(resp => resp.json())
+  .then(searchData => this.setState( {searchResults: searchData.results} ))
+}
+
+
+  handleOnChange = (event) => {
+    this.setState({
+      query: event.target.value
+    })
+  }
+  render() {
+    return(
+    <div className="searchable-movie-reviews">
+      <form  onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.query}
+            onChange={this.handleOnChange} />
+
+          <input type="submit"
+            value="Search Movie Reviews" />
+        </form>
+      <MovieReviews searchResults={this.state.reviews} />
+    </div>
+    )
+  }
+}
