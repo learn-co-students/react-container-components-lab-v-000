15213da185e42da1c5ebf5109b6552483e17e4eb
Monica Marziano <mmarziano1@gmail.com>
2020-01-03 10:38:53 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({reviews}) => {
+	return (
+		<div className="review-list">
+			{reviews.map((review, idx) => {
+				return (
+					<div className="review" key={idx}>
+						<h1>{review.display_title}</h1>
+						<h3>Opening Date: {review.opening_date}</h3>
+						<h2>{review.headline}</h2>
+						<p>{review.summary_short}</p>
+						<a href={review.link.url}>{review.link.suggested_link_text}</a>
+					</div>)
+			})}
+		</div>
+	)
+}
+
+export default MovieReviews
