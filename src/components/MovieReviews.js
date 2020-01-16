aa2386fb5034b095112f2287e7360f236dd72ad6
gitluky <jluk87@gmail.com>
2020-01-15 23:40:43 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -7,7 +7,7 @@ const MovieReviews = ({reviews}) => {
         return(
           <div className="review" key={review.display_title}>
             <h2>{review.display_title}</h2>,
-            <h3>{review.headh3ne}</h3>,
+            <h3>{review.headline}</h3>,
             <h4>{review.byline}</h4>,
             <p>{review.summary_short}</p>
           </div>
