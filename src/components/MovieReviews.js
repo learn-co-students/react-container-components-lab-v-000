c6472bb06cda7e6cb114130f12c92f6634aba112
lrh2daizzo <herrera.lrh@gmail.com>
2020-08-21 09:39:04 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
-// Code MovieReviews Here
+import React from 'react';
+import { head } from 'fetch-mock';
+
+const Review = ({
+    headline,
+    byline,
+    link,
+    summary_short }) => {
+        return (
+            <div key={headline} className='review'>
+                <header>
+                    <a className='review-link' href={link.url}></a>
+                    <br/>
+                    <span className="author">{byline}</span>
+                </header>
+                <blockquote>summary_short</blockquote>
+            </div>
+        )
+    }
+const MovieReviews = ({ reviews }) => <div className='review-list'>{reviews.map(Review)}</div>
+
+MovieReviews.defaultProps = {
+    reviews: []
+}
+export default MovieReviews; 
