fee168dfc76f0b5decfa41cfe24c7657b1b51842
jamieberrier <berrier.jamie@gmail.com>
2020-09-02 10:17:34 -0700

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,27 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+// fetch a list of the most recent reviews and display them.
+// have a top-level wrapping element with class latest-movie-reviews.
+class LatestMovieReviewsContainer extends Component {
+    state = {
+        reviews: []
+    }
+
+    componentDidMount() {
+        fetch(URL)
+        .then(response => response.json())
+        .then(movies => {this.setState({reviews: movies.results})})
+        .catch(error => alert(error.message))
+    }
+
+    render() {
+        return(
+            <div className="latest-movie-reviews">
+                <h1>Latest Movie Reviews</h1>
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
+export default LatestMovieReviewsContainer
