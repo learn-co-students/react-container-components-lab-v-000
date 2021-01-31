678bc697ac0d9236b2c68b4f240b82064dc05caf
RonSala <ron.sala@gmail.com>
2021-01-31 11:55:51 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
@@ -7,7 +7,7 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
-class LatestMovieReviewsContainer extends Component {
+class LatestMovieReviewsContainer extends PureComponent {
   state = {
     reviews: []
   }
