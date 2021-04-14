bd346e9f5f8f243aeceadd4da3e2c9f4f7d3e18f
github username <d24dust@hotmail.com>
2021-04-14 15:56:56 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,40 @@
+import React from "react";
 // Code MovieReviews Here
+
+const MovieReviews = (props)=> (
+  <div className="review-list">
+    {props.reviews.map((reviewData, index)=> {
+      const { headline, link, multimedia, summary_short } = reviewData
+      return(
+        <div className="review" key={index.toString()}>
+          <h4><a href={link.url}>{headline}</a></h4>
+          <img src={multimedia.src} alt="face" width={multimedia.width} height={multimedia.height}/>
+          <p>{summary_short}</p>
+        </div>
+      )
+    })}
+  </div>
+)
+export default MovieReviews
+/*   ______________________
+   /                        \
+  | R.I.P. Review component  |
+  |       2021-2021          |
+  |     Cause of rip:        |
+  |    bad test design       |
+  |                          |
+-----------------------------------------------------------
+
+const Review = ({data})=> {
+  const {headline, link, multimedia, summary_short} = data
+  return(
+    <div className="review">
+      <h4><a href={link.url}>{headline}</a></h4>
+      <img src={multimedia.src} alt="face"
+      width={multimedia.width}
+      height={multimedia.height} />
+      <p>{summary_short}</p>
+    </div>
+  )
+}
+-------------------------------------------------------------- */
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,28 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+  state = {
+    reviews: null,
+    searchTerm: "starwars"
+  }
+
+  handleSubmit = (event)=> {
+    event.preventDefault()
+    fetch(URL + `&query=${this.state.searchTerm}`).then(resp=>resp.json()).then(data=> this.setState({reviews: data.results}))
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={event=> this.setState({searchTerm: event.target.value})}/>
+          <input type="submit" />
+        </form>
+        {(this.state.reviews) ? <MovieReviews reviews={this.state.reviews} /> : <img src="https://i.imgur.com/dpBR607.jpg" alt="no" height="169px" width="169px" />}
+      </div>
+    );
+  }
+}
+
+export default SearchableMovieReviewsContainer;
