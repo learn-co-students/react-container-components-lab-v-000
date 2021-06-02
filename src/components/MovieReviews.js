a15d641a2632b1fa5082b6114f6b6d6969003a89
github username <github email address>
2021-06-02 10:59:54 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,25 @@
+import React from 'react';
 // Code MovieReviews Here
+
+const MovieReviews = ({reviews}) => {
+    return (
+  
+      reviews.map(review => {
+        return (
+        <div className="review">
+          <p><a className="review-link" href={review.link.url}>{review.headline}</a></p>
+          <p>By {review.byline}</p>
+          {console.log(review)}
+          <blockquote>{review.summary_short}</blockquote>
+        </div>
+        )
+      })
+    )
+
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
