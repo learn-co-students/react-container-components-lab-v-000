12ac797904d66a0fe218bc661c39116b944b1347
Laura Travin <laura.travin@gmail.com>
2021-06-05 08:22:42 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,29 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: []
+    }
+
+    componentDidMount() {
+        fetch(URL)
+        .then(response => response.json())
+        .then( movies => {this.setState({reviews: movies.results})})
+        .catch(error => console.log(error.message))
+    }
+
+    render(){
+        return(
+            <div className="latest-movie-reviews">
+            <h2>The Latest Reviews:</h2>
+           
+            <MovieReviews reviews={this.state.reviews} />
+          </div>
+
+        )
+    }
+
+}
+export default LatestMovieReviewsContainer
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,58 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+
+
+    state = {
+        searchTerm: '',
+        reviews: []
+    }   
+
+
+    handleInputSearch = e => {
+        this.setState(
+          //  {[e.target.name]: e.target.value}
+          {searchTerm: e.target.value}
+        )
+    }
+    //the submit do the fetch
+    handleSubmit = (e) => {
+        e.preventDefault()
+   
+        fetch(URL+`&query=${this.state.searchTerm}`)
+        .then(response => response.json())
+        .then(movies => {
+            this.setState( {reviews: movies.results, searchTerm: ''})
+        })
+
+
+
+    }
+
+
+    render(){
+        return(
+            <div>
+                    <form onSubmit={this.handleSubmit}>  
+                        <label>Search Movie Reviews</label>
+                        <input
+                        type="text"
+                        name="searchTerm"
+                        value={this.state.searchTerm}
+                        onChange={this.handleInputSearch}
+                        />
+                        <button type="submit">Search</button>
+                    </form>
+                    <div>
+                     <MovieReviews reviews={this.state.reviews} />
+                    </div>
+             </div>
+             
+              
+        )
+    }
+
+}
+
+export default SearchableMovieReviewsContainer;
